e171ac43c05c0b47e814ae31b507ae670c8a86d5
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-03 15:16:48 +0300

    Add main file with documentation

---
+++ Average/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (3/4/2020)
+Purpose: This file run the program that get 3 variables 
+	     from user and calculate their average.
+\********************************************************/
