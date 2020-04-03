16e2414702662a5e996e56c8c0f234b7a9d55087
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-03 15:27:53 +0300

    write the function that get number from user

--- Average/main.c
+++ Average/main.c
@@ -7,6 +7,11 @@ Purpose: This file run the program that get 3 variables
 
 #include <stdio.h>
 
+#define INITIAL_NUMBER (0)
+#define ENTER ('\n')
+#define DECIMAL (10)
+#define CHAR_ZERO ('0')
+
 int get_number_from_user() {
 	/********************************************************\
 	* Function name - get_number_from_user
@@ -24,4 +29,20 @@ int get_number_from_user() {
 	*
 	* Author - Liri
 	\********************************************************/
+	int number = INITIAL_NUMBER; /*the total number the user enter*/
+	int char_from_user; /*the next char from user*/
+
+	while ((char_from_user = getchar()) != ENTER) {
+		/*multiply the digits seen already by 10 and
+		add the decimal value of the new digit*/
+		number = number * DECIMAL + (char_from_user - CHAR_ZERO);
+	}
+	return number;
+}
+
+void main() {
+	int grade = INITIAL_NUMBER;
+	printf("Enter grade: ");
+	grade = get_number_from_user();
+	printf("Your number is %d", grade);
 }
