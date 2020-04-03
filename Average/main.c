a14c18a5615d96a00a8f436e231e3228f4cd7827
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-03 15:17:57 +0300

    add include to stdio.h library

--- Average/main.c
+++ Average/main.c
@@ -4,3 +4,6 @@ Author: Liri (3/4/2020)
 Purpose: This file run the program that get 3 variables 
 	     from user and calculate their average.
 \********************************************************/
+
+#include <stdio.h>
+
