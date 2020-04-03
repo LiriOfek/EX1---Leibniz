6bb1c7f2ee533a2a580665bbbef445b8608b8e4d
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-03 15:20:01 +0300

    create function that get number from user, and add its documentation

--- Average/main.c
+++ Average/main.c
@@ -7,3 +7,21 @@ Purpose: This file run the program that get 3 variables
 
 #include <stdio.h>
 
+int get_number_from_user() {
+	/********************************************************\
+	* Function name - get_number_from_user
+	*
+	* Function Purpose - get the number that the user type
+	*
+	* Parameters - OUT integer number - the number that the user type
+	*
+	* Return Value - the number that the user type
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function get the number that the user types
+	*			  and return it.
+	*
+	* Author - Liri
+	\********************************************************/
+}
