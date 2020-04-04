e895c52612f87576446134566a7662a31dcecd5e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-04 14:29:57 +0300

    add documentation to main file in calculator

---
+++ Calculator/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (4/4/2020)
+Purpose: This file run the program that get 2 numbers and operator
+		 from user and calculate the result of the expression.
+\********************************************************/
