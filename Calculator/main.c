fe20749fde380e197eb4fd34e556dcec893ac272
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-05 13:45:26 +0300

    use scanf to get numbers from user and remove obvious explaination about variables

--- Calculator/main.c
+++ Calculator/main.c
@@ -4,16 +4,16 @@ Author: Liri (4/4/2020)
 Purpose: This file run the program that get 2 numbers and operator
 		 from user and calculate the result of the expression.
 \********************************************************/
-#include "calculator_according_to_operator.h"
-
+#define _CRT_SECURE_NO_WARNINGS
 #define INITIAL_NUMBER (0)
 #define ENTER ('\n')
 #define DECIMAL (10)
 #define CHAR_ZERO ('0')
-
 #define STRING_FOR_ENTER_FIRST_NUMBER ("Enter first number:     ")
 #define STRING_FOR_ENTER_SECOND_NUMBER ("Enter second number:     ")
 #define STRING_FOR_ENTER_OPERATR ("Enter operator:     ")
+
+#include "calculator_according_to_operator.h"
 
 void main() {
 	/********************************************************\
@@ -33,20 +33,27 @@ void main() {
 	*
 	* Author - Liri
 	\********************************************************/
-	int first_number = INITIAL_NUMBER; /*the first number the user enter*/
-	int second_number = INITIAL_NUMBER; /*the second number the user enter*/
-	char operator = INITIAL_NUMBER; /*the operator the user enter*/
+	int first_number = INITIAL_NUMBER; 
+	int second_number = INITIAL_NUMBER; 
+	char operator = INITIAL_NUMBER; 
 	float result = INITIAL_NUMBER; /*the result of the expression*/
 
-								   /*get two numbers and operator from user*/
+	/*get two numbers and operator from user*/
 	printf(STRING_FOR_ENTER_FIRST_NUMBER);
-	first_number = get_number_from_user();
+	scanf("%d", &first_number);
 	printf(STRING_FOR_ENTER_SECOND_NUMBER);
-	second_number = get_number_from_user();
+	scanf("%d", &second_number);
+	/*catch the \n*/
+	getchar();
 	printf(STRING_FOR_ENTER_OPERATR);
 	operator = getchar();
 
 	/*calcullate the expression and print the result*/
-	result = calculate_expression(first_number, second_number, operator);
-	printf("%d%c%d==%f\n", first_number, operator, second_number, result);
+	result = calculate_expression(first_number, 
+									second_number, 
+									operator);
+	printf("%d%c%d==%f\n", first_number, 
+							operator, 
+							second_number, 
+							result);
 }
